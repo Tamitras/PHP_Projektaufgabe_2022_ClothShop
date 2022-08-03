f472adf8fed43344e7008aaa238034fd224b47db
Erik <erik.kaufmann91@gmail.com>
2022-08-03 15:44:36 +0200

    added bootstrap, adjusted layout (dark mode)

---
+++ webShop/index.php
@@ -0,0 +1,151 @@
+<?php
+
+// Fehleranalyse
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>WebShop 1390365 Erik Kaufmann</title>
+    <meta charset="utf-8">
+    <!-- <link media="all" type="text/css" rel="assets/bootstrap-5.0.2/css/bootstrap.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body onload="bodyLoaded()">
+    <div class="MainContainer">
+
+        <!-- Header -->
+        <!-- <div id="header"></div> -->
+        <?php include '../webShop/templates/header/header.php' ?>
+
+        <div class="container" style="color: white; background: rgb(35 35 35); border-radius:25px;">
+
+            <!-- Searchbar -->
+            <div class="container-fluid pt-2">
+                <form class="d-flex">
+                    <input style="background: rgb(35 35 35);" class="form-control me-2" type="search" placeholder="Suche nach Produkt" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+
+
+
+            <!-- content -->
+            <div class="row">
+
+                <?php for ($x = 0; $x <= 10; $x++) {
+
+                    // if($x % 2 == 0)
+                    // {
+                    //    echo "mmäööögg";
+                    // }
+                ?>
+                    <div class="col-xs-12 col-xl-3 col-lg-4">
+                        <div class="card m-2 p-3" style="background: rgb(23 23 23); border-radius:25px;">
+                            <div class="col border-3 buttonNoSelect">
+                                <img style="background: black; border-radius:25px;" src="assets/Shoes/orange.svg" class="card-img-top p-3">
+                            </div>
+                            <div class="col">
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Orange Shoes</h5>
+                                    <p class="card-text">Orange shoes directly from the moon</p>
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                    <a href="#" class="btn btn-success">In den Warenkorb</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </div>
+        </div>
+
+        <!-- footer -->
+        <div class="container mt-5">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                        <svg class="bi" width="30" height="24">
+                            <use xlink:href="#bootstrap"></use>
+                        </svg>
+                    </a>
+                    <span class="text-muted">© 2022 Kfm Shop, Inc</span>
+                </div>
+
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3"><a class="text-muted" href="#">
+                            <svg class="bi" width="24" height="24">
+                                <use xlink:href="#twitter"></use>
+                            </svg></a>
+                    </li>
+                    <li class="ms-3"><a class="text-muted" href="#">
+                            <svg class="bi" width="24" height="24">
+                                <use xlink:href="#instagram"></use>
+                            </svg></a>
+                    </li>
+                    <li class="ms-3"><a class="text-muted" href="#">
+                            <svg class="bi" width="24" height="24">
+                                <use xlink:href="#facebook"></use>
+                            </svg></a>
+                    </li>
+                </ul>
+            </footer>
+        </div>
+    </div>
+
+    <script src="https://use.fontawesome.com/43573797bc.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- <script src="assets/bootstrap-5.0.2/js/bootstrap.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+<style>
+    body {
+        /* overrides the default 8px margin  */
+        margin: 0 !important;
+        background-color: rgb(23, 23, 23);
+    }
+
+    .buttonNoSelect {
+        user-select: none;
+        -moz-user-select: none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        -o-user-select: none;
+
+        pointer-events: none;
+    }
+</style>
+
+<script>
+    // Rendert den Header (Logo, Warenkorb etc) via Javascript
+    // $(function() {
+    //     $("#header").load("templates/header/header.php");
+    // });
+
+    function bodyLoaded() {
+
+        // /../ --> navigates one level back or up 
+        // in this case from /index to /root(htdocs)
+        let url = "service/mainservice.php?action=test";
+
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function(response) {
+                // alert(response);
+                console.log("url", url);
+            }
+        });
+
+    }
+</script>
---
+++ webShop/templates/header/header.php
@@ -0,0 +1,14 @@
+<div class="container mt-5 mb-5 buttonNoSelect">
+    <div class="row justify-content-center">
+        <div class="col align-self-center">
+            <img class="rounded mx-auto d-block" src="/webShop/assets/Header/logo.svg" width="450" height="300">
+        </div>
+    </div>
+</div>
+
+<script>
+</script>
+
+<style>
+
+</style>
